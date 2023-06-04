0fc51cabc9fafaed5abb4110673d2814e75e823a
huanghao <1308936944@qq.com>
2023-06-04 19:11:30 +0800

    feat: three基本使用，gsap动画库基本使用

---
+++ src/practice/basic.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useRef } from "react";
+import * as Three from 'three'
+// 导入轨道控制器
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+// 使用gsap动画库
+import gsap from 'gsap'
+
+// 创建时钟
+const clock = new Three.Clock()
+// 创建场景
+const sence = new Three.Scene();
+// 场景相机
+const camera = new Three.PerspectiveCamera(
+  75,
+  window.innerWidth/window.innerHeight,
+  0.1,
+  1000
+)
+
+camera.position.set(0,0,10)
+sence.add(camera)
+// 添加物体
+const cubeGeometry = new Three.BoxGeometry(1,1,1)
+const cubeMaterial = new Three.MeshBasicMaterial({color: 0xffff00})
+const cube = new Three.Mesh(cubeGeometry, cubeMaterial)
+
+// 修改物体的位置
+// 方案1
+// cube.position.set(3, 0, 0)
+// 方案2
+// cube.position.x = 2
+
+// 物体缩放
+// cube.scale.set(x, y, z)
+cube.scale.x = 2
+
+// 物体旋转
+// x轴旋转45°
+// cube.rotation.set(Math.PI/4, 0, 0)
+
+sence.add(cube)
+
+// 初始化渲染器
+const renderer = new Three.WebGLRenderer()
+// 数值渲染的尺寸
+renderer.setSize(window.innerWidth, window.innerHeight)
+// document.body.appendChild()
+// 添加坐标轴辅助
+// 红色x，绿色y，蓝色z
+const axesHelper = new Three.AxesHelper(5)
+sence.add(axesHelper)
+
+// 使用gsap设置动画
+gsap.to(cube.position, {x: 5, duration: 5,
+    // 重复次数， -1无限次重复
+// repeat: -1,
+// 来回运动
+// yoyo: true
+})
+gsap.to(cube.rotation, {x: 2 * Math.PI, duration: 5, ease: "power1.in", 
+onStart(){
+    console.log('动画开始')
+},
+onComplete() {
+    console.log("动画完成")
+}})
+
+export default function() {
+  const container = useRef()
+
+  useEffect(() => {
+    container.current.appendChild(renderer.domElement)
+    refresh()
+    // 创建轨道控制器
+    // 使相机围绕目标进行轨道移动
+    // 长按鼠标左键移动查看
+    const controls = new OrbitControls(camera, renderer.domElement)
+  }, [])
+
+  //requestAnimationFrame调用 refresh 时会传入时间作为参数
+  // 可以根据这个参数做一个和时间匹配的动画
+  function refresh(time) {
+    // 获取时钟运行总时长
+    // const passedTime = clock.getElapsedTime()
+    // // 两次获取时间的间隔
+    // // const timeGap = clock.getDelta()
+    // const offset = passedTime % 5
+    // // const offset = time / 1000 % 5
+    // cube.position.x = offset
+    renderer.render(sence, camera)
+    window.requestAnimationFrame(refresh)
+  }
+
+  return <div ref={container}></div>
+}
