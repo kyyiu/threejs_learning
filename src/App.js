cc341f9aa67383e0b50ad3de6a6703e5b1d3c121
huanghao <1308936944@qq.com>
2023-06-21 11:37:34 +0800

    feat: hdr使用

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Basic from './practice/basic'
 import BufferGem from './practice/bufferGem'
 import BasicMaterial from './practice/material/basic'
 import VrHouse from './practice/vr_house/vrHouse'
+import Hdr from './practice/vr_house/hdr'
 export default function() {
-  return <VrHouse></VrHouse>
+  return <Hdr></Hdr>
 }
---
+++ src/practice/vr_house/hdr.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import * as Three from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader"
+const scene = new Three.Scene();
+const camera = new Three.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const geometry = new Three.SphereGeometry(5, 32, 32)
+const pic = require("./hdr/h.hdr")
+const texture = new RGBELoader().load(pic, (t) => {
+  const m = new Three.MeshBasicMaterial({map: t})
+  const sp = new Three.Mesh(geometry, m);
+  sp.geometry.scale(1, 1, -1);
+  scene.add(sp)
+})
+
+const renderer = new Three.WebGL1Renderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+export default function() {
+
+  useEffect(() => {
+    const dom = document.querySelector(".canvas");
+    const controls = new OrbitControls(camera, dom);
+    controls.enableDamping = true;
+    dom.appendChild(renderer.domElement);
+    animation();
+  }, []);
+
+  const animation = function() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(animation);
+  }
+
+  return <div className="canvas"></div>
+}
