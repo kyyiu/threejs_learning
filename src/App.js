7627799bcfde25a060a6aa3dbbee54fe32635206
huanghao <1308936944@qq.com>
2023-06-28 14:49:46 +0800

    feat: 新增路由

--- src/App.js
+++ src/App.js
@@ -21,9 +21,25 @@ function NavCom({path, imgUrl, desc}) {
 function Home() {
   return <div >
     <NavCom 
+      path={"/basic"}
+      imgUrl={require('./assets/img/react_logo.png')}
+      desc="threejs基础使用"/>
+    <NavCom 
+      path={"/buffer_gem"}
+      imgUrl={require('./assets/img/react_logo.png')}
+      desc="bufferGem对象使用"/>
+    <NavCom 
+      path={"/basic_material"}
+      imgUrl={require('./assets/img/react_logo.png')}
+      desc="基础材质的使用"/>
+    <NavCom 
       path={"/hdr"}
       imgUrl={require('./assets/img/react_logo.png')}
-      desc="hdr"/>
+      desc="hdr文件使用，并生成3d球体全景效果"/>
+    <NavCom 
+      path={"/vr_house"}
+      imgUrl={require('./assets/img/react_logo.png')}
+      desc="使用图片生成3d全景效果"/>
   </div>
 }
 
@@ -32,6 +48,10 @@ export default function() {
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/hdr" element={<Hdr/>}></Route>
+      <Route path="/vr_house" element={<VrHouse/>}></Route>
+      <Route path="/basic" element={<Basic/>}></Route>
+      <Route path="/buffer_gem" element={<BufferGem/>}></Route>
+      <Route path="/basic_material" element={<BasicMaterial/>}></Route>
     </Routes>
   </div>
 }
