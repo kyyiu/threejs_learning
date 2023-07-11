fc73da994d13dd268e5fd17dc7c2a997dfa96061
huanghao <1308936944@qq.com>
2023-07-11 17:13:47 +0800

    feat: canvas涂鸦板

--- src/practice/interactive/graffiti.js
+++ src/practice/interactive/graffiti.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from "react";
 import * as Three from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 const sence = new Three.Scene();
 const camera = new Three.PerspectiveCamera(
@@ -14,20 +13,41 @@ camera.position.set(0,0,10)
 sence.add(camera)
 
 const renderer = new Three.WebGLRenderer()
-const controls = new OrbitControls(camera, renderer.domElement)
-// 设置阻尼，让控制器更真实, 必须在动画循环调用update方法
-controls.enableDamping = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 
+let canvasGeo, canvasContext
 export default function() {
     const container = useRef()
   
     useEffect(() => {
+      const canvas = document.createElement("canvas")
+      canvas.width = 1024
+      canvas.height = 1024
+      canvasContext = canvas.getContext('2d')
+      canvasContext.fillStyle="#ffffff"
+      canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+      const texture = new Three.Texture(canvas)
+      const g = new Three.PlaneGeometry(12, 12, 4)
+      const m = new Three.MeshBasicMaterial({map: texture, transparent: true, opacity: 1, color: 0xffffff})
+      canvasGeo = new Three.Mesh(g, m)
+      sence.add(canvasGeo)
       container.current.appendChild(renderer.domElement)
       refresh()
     }, [])
 
     function refresh(time) {
+      if (canvasGeo) {
+        canvasGeo.material.map.needsUpdate = true;
+        canvasContext.fillStyle = "rgba("
+          +Math.floor(Math.random()*255) + ","
+          +Math.floor(Math.random()*255) + ","
+          +Math.floor(Math.random()*255) + ","
+          +(Math.random() - .1) + ")";
+        canvasContext.beginPath();
+        canvasContext.arc(1024 * Math.random(), 1024*Math.random(),10 + Math.random() * 50,0,Math.PI*2,true);
+        canvasContext.closePath();
+	      canvasContext.fill();
+      }
       renderer.render(sence, camera)
       window.requestAnimationFrame(refresh)
     }
--- src/router/interactive.js
+++ src/router/interactive.js
@@ -6,5 +6,6 @@ import { createRouteItem } from './tools/createItem'
 
 export const interactiveRoute = [
   createRouteItem("/click_item", <ClickItem/>, "物体点击", require('../assets/img/react_logo.png')),
-  createRouteItem("/pop_dialog", <PopDialog/>, "鼠标处理物体检测", require('../assets/img/react_logo.png'))
+  createRouteItem("/pop_dialog", <PopDialog/>, "鼠标处理物体检测", require('../assets/img/react_logo.png')),
+  createRouteItem("/graffiti", <Graffiti/>, "canvas纹理涂鸦板", require('../assets/img/react_logo.png'))
 ]
