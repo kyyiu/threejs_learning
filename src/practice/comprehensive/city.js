b4997fc8cc24ab3454bd2400036b4484ddc6dcc1
huanghao <1308936944@qq.com>
2023-11-07 10:19:40 +0800

    feat: 城市模型点击

--- src/practice/comprehensive/city.js
+++ src/practice/comprehensive/city.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as Three from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
@@ -16,7 +16,10 @@ const camera = new Three.PerspectiveCamera(
 camera.position.set(1000, 1000, 1000)
 sence.add(camera)
 
-const renderer = new Three.WebGLRenderer()
+const renderer = new Three.WebGLRenderer({
+  antialias: true, // 抗锯齿
+  logarithmicDepthBuffer: true, // 防止地面和物体过近出现闪烁
+})
 const controls = new OrbitControls(camera, renderer.domElement)
 // 设置阻尼，让控制器更真实, 必须在动画循环调用update方法
 controls.enableDamping = true
@@ -24,13 +27,17 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 const self = {}
 export default function () {
   const container = useRef()
-
+  const [clicked, setClicked] = useState([])
   useEffect(() => {
     window.addEventListener('resize', onWindowResize, false);//窗口变化监听
     init()
     container.current.appendChild(renderer.domElement)
     refresh()
   }, [])
+  useEffect(() => {
+    const div = document.querySelector('.panel'); 
+    div.scrollTop = div.scrollHeight; 
+  }, [clicked])
 
   function init() {
     self.loader = new GLTFLoader()
@@ -49,12 +56,20 @@ export default function () {
     renderer.shadowMap.enabled = true
     renderer.toneMapping = Three.ACESFilmicToneMapping
     renderer.toneMappingExposure = 1.5
+    initLight()
+  }
+
+  function initLight() {
+    const light = new Three.DirectionalLight(0xffffff, 1)
+    light.position.set(10,100,10)
   }
 
   function onWindowResize() {
     const width = window.innerWidth;
     const height = window.innerHeight;
-
+    // raycaster只对mesh对象有感应，而导入的模型基本是group的，
+    // 所以intersectObject的第二个参数必须为true，要检查后代才能拿到该模型。
+    // 如果目标模型的同级模型干扰严重，也可以采用在模型外部新建一个透明的mesh进行包裹绑定
     camera.aspect = width / height;//获得当前摄像机缩放比
     camera.updateProjectionMatrix();//更新矩阵
     renderer.setSize(width, height);
@@ -65,5 +80,33 @@ export default function () {
     window.requestAnimationFrame(refresh)
   }
 
-  return <div ref={container}></div>
+  function handleClick(e) {
+    const raycaster = new Three.Raycaster()
+    const pointer   = new Three.Vector2()
+    // 将鼠标位置归一化为设备坐标。x 和 y 方向的取值范围是 (-1 to +1)
+    pointer.x = (e.clientX / window.innerWidth) * 2 -1
+    pointer.y = -(e.clientY/window.innerHeight) * 2 +1 
+    raycaster.setFromCamera(pointer, camera)
+    // 计算物体和射线的焦点
+    const intersects = raycaster.intersectObjects( sence.children );
+    // for (let i = 0; i < intersects.length; i++) {
+    //   intersects[i].object.material.color.set(0xff0000);
+    // }
+    if (intersects.length) {
+      setClicked([...clicked, intersects[0].object.name])
+    }
+    console.log("click---", intersects)
+  }
+
+  return <div style={{position: 'relative'}}>
+    <div ref={container} onClick={handleClick}></div>
+    <div className="panel" style={{position: 'absolute', right: 0, top: 0, height: '300px', width: '200px', overflow: 'auto', background: '#fff'}}>
+      <div>点击记录</div>
+      {
+        clicked.map((e, i) => {
+          return <div key={i}>点击了{e}</div>
+        })
+      }
+    </div>
+  </div>
 }
