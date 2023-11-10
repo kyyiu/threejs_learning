cfdd40e13ea1b89623aa6b3af61bac139c788ae3
huanghao <1308936944@qq.com>
2023-11-10 14:08:37 +0800

    feat: shader发射轨迹

--- src/practice/comprehensive/shader/city2/flyLine.js
+++ src/practice/comprehensive/shader/city2/flyLine.js
@@ -20,13 +20,31 @@ export default class FlyLineShader {
     // 设置几何体顶点属性
     this.geometry.setAttribute('aSize', new THREE.BufferAttribute(aSizeArray, 1))
     this.shaderMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        uTime: {
+          value: 0
+        },
+        uColor: {
+          value: new THREE.Color(0xffff00)
+        },
+        uLength: {
+          value: aSizeArray.length
+        }
+      },
       vertexShader: `
         attribute float aSize;
         varying float vSize;
+        uniform float uTime;
+        uniform float uLength;
+        uniform vec3 uColor;
         void main() {
           vec4 viewPosition = viewMatrix * modelMatrix * vec4(position, 1);
           gl_Position = projectionMatrix * viewPosition;
-          vSize = (aSize - 500.0)*0.1;
+          vSize = (aSize - uTime);
+          if (vSize < 0.0) {
+            vSize = vSize + uLength;
+          }
+          vSize = (vSize - 100.0)* 0.1;
           // gl_PointSize = vSize;
           // vSize上面为固定值，远离为负值，取反,效果为 远离细，靠近粗
           gl_PointSize = -(vSize/viewPosition.z);
@@ -49,5 +67,11 @@ export default class FlyLineShader {
       blending: THREE.AdditiveBlending
     })
     this.mesh = new THREE.Points(this.geometry, this.shaderMaterial)
+    gsap.to(this.shaderMaterial.uniforms.uTime, {
+      value: 1000,
+      duration: 1,
+      repeat: -1,
+      ease: 'none'
+    })
   }
 }
